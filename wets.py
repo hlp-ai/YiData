2c4154c49c58bf678c72bc45290810f56f61e5a2
liuxf_hust <hlp_ai@sina.com>
2025-05-26 17:43:06 +0800

    添加WET文件内容读取。

--- wets.py
+++ wets.py
@@ -1,6 +1,8 @@
 import os
 
-from utils import download, ungzip
+from warcio import ArchiveIterator
+
+from utils import download, ungzip, download_progress
 
 cc_data_url = "https://data.commoncrawl.org/crawl-data/"
 cc_base_url = "https://data.commoncrawl.org/"
@@ -32,13 +34,47 @@ def get_wet_paths(cc_archive_id, out_dir=None):
     cc_wet_paths_path = os.path.join(out_dir, cc_wet_paths)
     ungzip(cc_wet_paths_gz_path, cc_wet_paths_path)
 
-    # read WET files
-    wetf = open(cc_wet_paths_path, encoding="utf-8")
-    wet_paths = [cc_base_url+line.strip() for line in wetf]  # 绝对化URL
 
-    return wet_paths
+def get_wet_name(wet_url):
+    parts = wet_url.split("/")
+    gz_path = parts[-1]
+    idx = gz_path.find(".gz")
+    return gz_path, gz_path[:idx]
+
+
+def get_text(wet_url, lang):
+    wet_gz_name, wet_name = get_wet_name(wet_url)
+    print("下载WET文件...")
+    download_progress(wet_url, wet_gz_name)
+
+    print("解压WET文件...")
+    ungzip(wet_gz_name, wet_name)
+
+    with open(wet_name, 'rb') as stream:
+        total_pages = 0
+        found = 0
+        docs = {}
+        for record in ArchiveIterator(stream):
+            if record.rec_type == 'conversion' and record.rec_headers.get_header('Content-Type').find('text/')==0:
+                total_pages += 1
+                langs = record.rec_headers.get_header("WARC-Identified-Content-Language")
+                url = record.rec_headers.get_header("WARC-Target-URI")
+                content_len = int(record.rec_headers.get_header("Content-Length"))
+                if langs is not None:
+                    langs = langs.split(",")
+                    content = record.content_stream().read().decode("utf-8")
+                    if lang==langs[0]:
+                        found += 1
+                        print("{}/{}".format(found, total_pages))
+                        docs[url] = content
+
+        return docs
 
 
 if __name__ == "__main__":
-    cc_id = "CC-MAIN-2025-05"
-    get_wet_paths(cc_id)
+    # cc_id = "CC-MAIN-2025-05"
+    # get_wet_paths(cc_id)
+
+    wet_url = cc_base_url + "crawl-data/CC-MAIN-2025-05/segments/1736703361941.29/wet/CC-MAIN-20250126135402-20250126165402-00000.warc.wet.gz"
+    docs = get_text(wet_url, "zho")
+    print(docs)
